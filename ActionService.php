2b5a7de4a35e79e1718340e8799abf463becac7b
Tojiddin Khamidov <khamidovtojiddin20@gmail.com>
2022-07-01 00:13:03 +0500

    Alif Job Task with OOP

---
+++ ActionService.php
@@ -0,0 +1,63 @@
+<?php 
+	class ActionService {
+		public function add(string $fileName, Product $product): string {
+			$fp = fopen($fileName, "a");
+			fwrite($fp, $product->getNameDashPrice());
+			fclose($fp);
+			return "Added product {$product->getNameDashPrice()}";
+		}
+
+		public function edit(string $fileName, int $productNumber): string {
+			$data = file($fileName);
+			$products = [];
+
+			foreach($data as $key => $line) {
+			    if ($key + 1 == $productNumber) {
+			    	$input = fopen("php://stdin", "rb");
+			    	echo "Enter product name ";
+			    	$name = trim(fgets($input));
+			    	echo "Enter product price ";
+			    	$price = trim(fgets($input));
+			    	fclose($input);
+			        $products[] = (new Product($name, $price))->getNameDashPrice();
+			    } else {
+			    	$products[] = $line;			    
+			    }
+			}
+
+			$fp = fopen($fileName, "w+");
+			foreach($products as $line) fwrite($fp, $line);
+			fclose($fp);  
+
+			return "Edited product with number $productNumber";
+		}
+
+		public function delete(string $fileName, int $productNumber): string {
+			 $data = file($fileName);
+			 $products = [];
+
+			 foreach($data as $key => $line) {
+			     if($key + 1 != $productNumber) {
+			         $products[] = $line;
+			     }
+			 }
+
+			 $fp = fopen($fileName, "w+");
+			 foreach($products as $line) fwrite($fp, $line);
+			 fclose($fp);  
+
+			 return "Deleted product with number $productNumber";
+		}
+
+		public function sum(string $fileName): string {
+			$data = file($fileName);
+			$sum = 0;
+
+			foreach($data as $key => $line) {
+				$sum += end(explode(" ", trim($line)));
+			}
+
+			return "Sum of all product prices is $sum";
+		}
+	}
+ ?>
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php 
+	include('Product.php');
+	include('ActionService.php');
+
+	function run() {
+		$input = fopen('php://stdin', 'rb');
+		echo "Enter filename ";
+		$fileName = trim(fgets($input));
+		
+		if (file_exists($fileName)) {			
+			echo "Enter action ";		
+			$action = trim(fgets($input));		
+			$actionService = new ActionService();
+
+			switch ($action) {
+				case 'add':
+					echo "Enter product name ";
+					$name = fgets($input);
+					echo "Enter product price ";
+					$price = fgets($input);
+					echo $actionService->add('data.txt', new Product($name, $price));
+
+					break;
+				case 'edit':
+					echo "Enter product number ";
+					$productNumber = fgets($input);
+					echo $actionService->edit($fileName, $productNumber);				
+					break;
+				case 'delete':
+					echo "Enter product number ";
+					$productNumber = fgets($input);
+					echo $actionService->delete($fileName, $productNumber);
+
+					break;
+				case 'sum':
+					echo $actionService->sum($fileName);					
+					break;
+				default:
+					echo "Incorrect action $action";
+					break;
+			}
+
+			fclose($input);
+		} else {
+			echo "There is no file $fileName";
+		}
+
+	};
+
+	run();
+ ?>
